245e6a9a1661f2ec0a08777e3bed47997b742a98
Vi Tran <contact.vitran@gmail.com>
2021-11-21 14:40:31 +1100

    Challenge 2 and 3 complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 
 const imagesContainer = document.querySelector('#dog-image-container');
+const breedsList = document.querySelector('#dog-breeds');
 const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
 const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
@@ -10,16 +11,30 @@ const getData = async (url) => {
     return data;
 }
 
-const createHtml = (item) => {
+const createImgHtml = (item) => {
     return `<img src="${item}" height="300px" alt="dog">`;
 }
 
+const createListHtml = (item) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${item}`;
+    listItem.style = "";
+    listItem.onclick = function changeColor() {
+        listItem.style = "color:orange"
+    };
+    return listItem;
+}
 
 
 window.addEventListener('DOMContentLoaded', async (event) => {
-    const serverResponse = await getData(imgUrl);
-    const dogImages = serverResponse.message;
+    const serverResponseImg = await getData(imgUrl);
+    const dogImages = serverResponseImg.message;
     dogImages.forEach((item) => {
-        imagesContainer.innerHTML += createHtml(item);
+        imagesContainer.innerHTML += createImgHtml(item);
     });
+    const serverResponseBreeds = await getData(breedUrl);
+    const dogBreeds = serverResponseBreeds.message;
+    for (const name in dogBreeds) {
+        breedsList.appendChild(createListHtml(name));
+    }
 })
