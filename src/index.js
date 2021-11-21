134f7c6b32ff24e7fc13a4b1fad14dfc445b5fe7
Vi Tran <contact.vitran@gmail.com>
2021-11-21 12:51:13 +1100

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+const imagesContainer = document.querySelector('#dog-image-container');
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+const getData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+const createHtml = (item) => {
+    return `<img src="${item}" height="300px" alt="dog">`;
+}
+
+
+
+window.addEventListener('DOMContentLoaded', async (event) => {
+    const serverResponse = await getData(imgUrl);
+    const dogImages = serverResponse.message;
+    dogImages.forEach((item) => {
+        imagesContainer.innerHTML += createHtml(item);
+    });
+})
